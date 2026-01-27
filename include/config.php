aa61121d61866e83e60910fa57909f7afa86d6f6
Lorenzo Di Battista <lorenzodibattista1@gmail.com>
2026-01-27 10:13:18 +0100

    Add configuration file

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Database connection parameters
+    define('DB_NAME', 'dbname');
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', 'dbuser');
+    define('DB_PASSWORD', 'dbpassword');
+?>
