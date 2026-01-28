9eacda66ae9aea3dec8b2b6554fc3082f6e39905
Lorenzo Di Battista <lorenzodibattista1@gmail.com>
2026-01-28 15:48:05 +0100

    Implement singleton pattern for FPersistentManager and FConnectionDB

--- Foundation/FConnectionDB.php
+++ Foundation/FConnectionDB.php
@@ -2,6 +2,24 @@
 
 class FConnectionDB {
     
+    private static $instance;
+    private $dbh;
+
+    private function __construct() {
+        try {
+            $this->dbh = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+        } catch(PDOException $e) {
+            print('ERROR: ' . $e->getMessage());
+            exit;
+        }
+    }
+
+    public static function getInstance() : FConnectionDB {
+        if(!isset(self::$instance)) {
+            self::$instance = new FConnectionDB();
+        }
+        return self::$instance;
+    }
 }
 
 ?>
--- Foundation/FPersistentManager.php
+++ Foundation/FPersistentManager.php
@@ -2,6 +2,19 @@
 
 class FPersistentManager {
 
+    private static $instance;
+
+    private function __construct() {
+
+    }
+
+    public static function getInstance() : FPersistentManager {
+        if(!isset(self::$instance)) {
+            self::$instance = new FPersistentManager();
+        }
+        return self::$instance;
+    }
+
 }
 
 ?>
