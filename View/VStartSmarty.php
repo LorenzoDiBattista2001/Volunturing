aedc5e6ddde28f66140828f70feaaa4aa503fa4a
Lorenzo Di Battista <lorenzodibattista1@gmail.com>
2026-02-27 12:40:24 +0100

    Install and setup Smarty
    Install Smarty latest distribution by means of Composer.
    Setup Smarty working directories. Create a Smarty configuration
    class in package View. Update .gitignore

---
+++ View/VStartSmarty.php
@@ -0,0 +1,17 @@
+<?php
+
+class VStartSmarty {
+    
+    public static function configuration() {
+        $smarty = new \Smarty\Smarty();
+
+        $smarty->setTemplateDir(__DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
+        $smarty->setCompileDir(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'templates_c' . DIRECTORY_SEPARATOR);
+        $smarty->setConfigDir(__DIR__ . DIRECTORY_SEPARATOR . 'configs' . DIRECTORY_SEPARATOR);
+        $smarty->setCacheDir(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR);
+
+        return $smarty;
+    }
+}
+
+?>
