1ea78d90c3940c9f6e8e5c98d811d77e6e8fc1b8
Lorenzo Di Battista <lorenzodibattista1@gmail.com>
2026-03-09 09:20:13 +0100

    Create utility class 'USession'

---
+++ Utility/USession.php
@@ -0,0 +1,43 @@
+<?php
+
+class USession {
+
+    private static $instance;
+
+    private function __construct() {
+        ini_set('session.gc_maxlifetime', GC_MAX_LIFETIME);
+        session_start();
+    }
+
+    public static function getInstance() : USession {
+        if(!isset(self::$instance)) {
+            self::$instance = new USession();
+        }
+        return self::$instance;
+    }
+
+    public function setSessionElement($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public function getSessionElement($key) {
+        return $_SESSION[$key];
+    }
+
+    public function isElementSet($key) : bool {
+        return isset($_SESSION[$key]);
+    }
+
+    public function getSessionStatus() {
+        session_status();
+    }
+
+    public function unsetSessionVariables() {
+        session_unset();
+    }
+
+    public function destroySession() {
+        session_destroy();
+    }
+}
+?>
--- include/config.php
+++ include/config.php
@@ -1,8 +1,11 @@
 <?php
 
-    //Database connection parameters
+    // Database connection parameters
     define('DB_NAME', 'dbname');
     define('DB_HOST', '127.0.0.1');
     define('DB_USER', 'dbuser');
     define('DB_PASSWORD', 'dbpassword');
+
+    // Session parameters
+    define('GC_MAX_LIFETIME', 3600);
 ?>
